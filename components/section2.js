ae0e37b81a088dcb8d6ec03e619f972c97ac36e7
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-07-24 22:11:33 +0900

    add spinner, error component in section2

--- components/section2.js
+++ components/section2.js
@@ -1,58 +1,80 @@
 import Link from "next/link"
 import Image from "next/image"
 import Author from "./_child/author"
+import fetcher from "../lib/fetcher";
+import Spinner from "./_child/spinner";
+import Error from "./_child/error";
 
 export default function section1() {
+  const { data, isLoading, isError } = fetcher('/api/posts');
+
+  if (isLoading) {
+    return (
+      <Spinner></Spinner>
+    )
+  }
+
+  if (isError) {
+    return <Error></Error>;
+  }
+
   return (
     <section className="container mx-auto md:px-20 py-10">
       <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
 
       {/* grid columns */}
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+        {Array.isArray(data) &&
+          data.map((value, index) => (
+            <Post data={value} key={"section2-post-" + index}></Post>
+          ))}
       </div>
     </section>
-  )
+  );
 }
 
-function Post() {
+function Post({data}) {
+  const {id, title, description, category, img, published, author} = data;
+
   return (
     <div className="item">
       <div className="images">
         <Link href={"/"}>
           <a>
-            <Image src={"/images/banner_img_01.jpg"} className="rounded" width={500} height={350}/>
+            <Image
+              src={img || "/"}
+              className="rounded"
+              width={500}
+              height={350}
+            />
           </a>
         </Link>
       </div>
       <div className="info flex justify-center flex-col py-4">
         <div className="cat">
           <Link href={"/"}>
-            <a className="text-orange-600 hover:text-orange-800">Business, Travel</a>
+            <a className="text-orange-600 hover:text-orange-800">
+              {category || "Unknown"}
+            </a>
           </Link>
           <Link href={"/"}>
-            <a className="text-gray-600 hover:text-gray-800">- July, 2022</a>
+            <a className="text-gray-600 hover:text-gray-800">
+              - {published || "Unknown"}
+            </a>
           </Link>
         </div>
 
         <div className="title">
           <Link href={"/"}>
             <a className="text-xl font-bold text-gray-800 hover:text-gray-600">
-              Your most unhappy customers are your greatest source of learning
+              {title || "Unknown"}
             </a>
           </Link>
         </div>
 
-        <p className="text-gray-500 py-3">
-          Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem lpsum decided to leave for the far World of Grammer.
-        </p>
-        <Author/>
+        <p className="text-gray-500 py-3">{description || "Unknown"}</p>
+        {author ? <Author /> : <></>}
       </div>
     </div>
-  )
+  );
 }
