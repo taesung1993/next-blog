86d01391146d77059c26ddd3d96d59f3e4789679
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-07-24 14:03:26 +0900

    publish initial section 4

--- components/section3.js
+++ components/section3.js
@@ -5,7 +5,7 @@ import Author from './_child/author';
 
 export default function section3() {
   return (
-    <section className="container mx-auto md:px-20 py-10">
+    <section className="container mx-auto md:px-20 py-16">
       <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
 
       {/* swiper */}
